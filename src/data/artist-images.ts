3ebf45715fb71c3fcffcd986f14047d194f98e85
katendepeterson29 <katendepeterson6@gmail.com>
2026-07-30 19:15:49 +0300

    add logo assets and update artist-images import

--- src/data/artist-images.ts
+++ src/data/artist-images.ts
@@ -1,5 +1,5 @@
 import img1 from "../../assets/artist/IMG_1004.JPG.jpeg";
-import img2 from "../../assets/img2.jpeg";
+import img2 from "../../assets/logo1.jpeg";
 // import img3 from "../../assets/img3.jpeg";
 // import img4 from "../../assets/img4.jpeg";
 // import img5 from "../../assets/img5.jpeg";
