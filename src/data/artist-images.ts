73fe2a81b6964eda4959ae4801262f521ad4ac1d
katendepeterson29 <katendepeterson6@gmail.com>
2026-07-30 18:51:35 +0300

    refactor artist-images import statements for clarity

--- src/data/artist-images.ts
+++ src/data/artist-images.ts
@@ -1,14 +1,14 @@
-import img1 from "../../assets/img1.png";
+import img3 from "../../assets/img1.png";
 import img2 from "../../assets/img2.jpeg";
-import img3 from "../../assets/img3.jpeg";
-import img4 from "../../assets/img4.jpeg";
-import img5 from "../../assets/img5.jpeg";
-import img6 from "../../assets/img6.png";
-import img7 from "../../assets/img7.png";
-import img8 from "../../assets/img8.jpeg";
-import img9 from "../../assets/img9.jpeg";
-import img10 from "../../assets/img10.png";
-import img11 from "../../assets/artist/IMG_1004.JPG.jpeg";
+// import img3 from "../../assets/img3.jpeg";
+// import img4 from "../../assets/img4.jpeg";
+// import img5 from "../../assets/img5.jpeg";
+// import img6 from "../../assets/img6.png";
+// import img7 from "../../assets/img7.png";
+// import img8 from "../../assets/img8.jpeg";
+// import img9 from "../../assets/img9.jpeg";
+// import img10 from "../../assets/img10.png";
+import img1 from "../../assets/artist/IMG_1004.JPG.jpeg";
 
 // Reuse the supplied artist photography cyclically so every artist has a local image.
 export const artistImages = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11];
